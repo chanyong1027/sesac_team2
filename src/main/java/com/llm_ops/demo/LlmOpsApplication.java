a0d02cbb7cb32efc5821640db64942a64a0d6138
chanyong <ghdcksdyd456@naver.com>
2026-01-10 16:50:41 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/llm_ops/demo/LlmOpsApplication.java
@@ -0,0 +1,13 @@
+package com.llm_ops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llm_ops/demo/LlmOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm_ops.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
