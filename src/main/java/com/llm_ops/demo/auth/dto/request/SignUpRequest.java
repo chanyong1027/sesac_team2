abeaeeab8f492fe575ca72088512b5b25254d4d9
Joooooonha <wnsgk517@naver.com>
2026-01-15 21:19:24 +0900

    feat : signup - password condition regex refactoring #7

--- src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
+++ src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
@@ -5,20 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record SignUpRequest (
-        @NotBlank
-        @Size(max = 50)
-        @Email
-        String email,
+public record SignUpRequest(
+                @NotBlank @Size(max = 50) @Email String email,
 
-        @NotBlank
-        @Size(min = 8, max = 20) //최대 길이를 20으로 설정했는데 hashing된 결과가 20을 넘어서 에러가 발생했었다.
-        @Pattern(
-                regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z\\\\d@$!%*#?&]{8,20}$",
-                message = "비밀번호는 영문/숫자/특수기호를 포함해야 합니다.(허융 특수 기호 : @ $ ! % * # ? &)")
-        String password,
+                @NotBlank @Size(min = 8, max = 20) // 최대 길이를 20으로 설정했는데 hashing된 결과가 20을 넘어서 에러가 발생했었다.
+                @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+                        message = "비밀번호는 영문/숫자/특수기호를 포함해야 합니다.(허융 특수 기호 : @ $ ! % * # ? &)"
+                )
+                String password,
 
-        @NotBlank
-        @Size(min = 2, max = 50)
-        String userName
+                @NotBlank @Size(min = 2, max = 50)
+                String name
 ){}
