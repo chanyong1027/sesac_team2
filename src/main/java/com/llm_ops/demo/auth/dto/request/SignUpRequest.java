063fa2178ff4d25a1fcb943e7af7045d5d76ce53
Joooooonha <wnsgk517@naver.com>
2026-01-15 20:25:33 +0900

    feat: signup - @Email annotation add, Domain name modified(userName > name) #7

--- src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
+++ src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
@@ -15,8 +15,7 @@ public record SignUpRequest (
         @Size(min = 8, max = 20) //최대 길이를 20으로 설정했는데 hashing된 결과가 20을 넘어서 에러가 발생했었다.
         @Pattern(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z\\\\d@$!%*#?&]{8,20}$",
-                message = "비밀번호는 영문/숫자/특수기호를 포함해야 합니다.(허융 특수 기호 : @ $ ! % * # ? &)"
-        )
+                message = "비밀번호는 영문/숫자/특수기호를 포함해야 합니다.(허융 특수 기호 : @ $ ! % * # ? &)")
         String password,
 
         @NotBlank
