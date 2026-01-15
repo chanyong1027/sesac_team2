5447b2d8882aa8d06c840029788045aa3061d1d6
Joooooonha <wnsgk517@naver.com>
2026-01-15 20:00:35 +0900

    EEmail 유효성 조건 추가

--- src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
+++ src/main/java/com/llm_ops/demo/auth/dto/request/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.llm_ops.demo.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -7,16 +8,16 @@ import jakarta.validation.constraints.Size;
 public record SignUpRequest (
         @NotBlank
         @Size(max = 50)
+        @Email
         String email,
 
         @NotBlank
-        @Size(min = 8, max = 100) //최대 길이를 20으로 설정했는데 hashing된 결과가 20을 넘어서 에러가 발생했었다.
+        @Size(min = 8, max = 20) //최대 길이를 20으로 설정했는데 hashing된 결과가 20을 넘어서 에러가 발생했었다.
         @Pattern(
-                regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$",
-                message = "비밀번호는 영문과 숫자를 포함해야 합니다."
+                regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z\\\\d@$!%*#?&]{8,20}$",
+                message = "비밀번호는 영문/숫자/특수기호를 포함해야 합니다.(허융 특수 기호 : @ $ ! % * # ? &)"
         )
-        String passwordHash,
-
+        String password,
 
         @NotBlank
         @Size(min = 2, max = 50)
