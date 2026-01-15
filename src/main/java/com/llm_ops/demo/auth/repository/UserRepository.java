3e77cb72006c3e4887f074cad3ddef772dafedf6
Joooooonha <wnsgk517@naver.com>
2026-01-15 21:20:03 +0900

    feat: signup - method type miss correction #7

--- src/main/java/com/llm_ops/demo/auth/repository/UserRepository.java
+++ src/main/java/com/llm_ops/demo/auth/repository/UserRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String userName);
-    boolean existsByUserName(String userName);
+    Optional<User> findByName(String name);
+
+    boolean existsByName(String name);
+
     boolean existsByEmail(String email);
 }
