227b611d63c0ef591ba2a33da564c0e107006415
Joooooonha <wnsgk517@naver.com>
2026-01-29 14:50:01 +0900

    refactor: log - PK값으로 UUID 대신 Long 수정 #72

--- src/main/java/com/llm_ops/demo/logs/repository/RequestLogRepository.java
+++ src/main/java/com/llm_ops/demo/logs/repository/RequestLogRepository.java
@@ -1,13 +1,12 @@
 package com.llm_ops.demo.logs.repository;
 
 import com.llm_ops.demo.logs.domain.RequestLog;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * RequestLog 엔티티에 대한 데이터 접근 레이어.
  */
-public interface RequestLogRepository extends JpaRepository<RequestLog, UUID> {
+public interface RequestLogRepository extends JpaRepository<RequestLog, Long> {
 
     // 추후 필요에 따라 쿼리 메소드 추가 예정
     // 예: List<RequestLog> findByWorkspaceIdAndCreatedAtBetween(Long workspaceId,
